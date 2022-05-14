fc4f0a877de6cd7ed54c7d402c41bfd0bfaf27e1
km8oz <ouazmir@gmail.com>
2022-05-14 17:52:07 +0100

    fix test

--- src/main.test.ts
+++ src/main.test.ts
@@ -3,9 +3,7 @@ import { GetOrderDataResponce } from './types';
 
 test('testing ApplicationWithPrepaymentGetOrderData', () => {
     // if(YandexDeliveryController) test.skip("all good")
-    let YandexApi = new YandexDeliveryController({
-        key: process.env.APIKEY
-    })
+    let YandexApi = new YandexDeliveryController();
     YandexApi.ApplicationWithPrepaymentGetOrderData({
         claim_id: "0ab1c23d4e5678f901g234h45678i01"
     }).then((res: any)=>{
