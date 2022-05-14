184a33b917a7cc8ab121eac13a5a40276f75e5e6
Buttmoon <lonokil48@gmail.com>
2022-05-14 05:03:28 +0300

    beta

--- src/main.test.ts
+++ src/main.test.ts
@@ -1,5 +1,5 @@
 import YandexDeliveryController from './main'
-//this is response the errors
+//this is response the errors‹‹
 test('testing class export', () => {
     if(YandexDeliveryController) test.skip("all good")
 });
